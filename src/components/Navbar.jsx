cb0c9ae1b28f83fee0307bc9c8974572275ef568
Debasish Pothal <debasish_pothal@hotmail.com>
2025-07-07 19:31:45 +0530

    added blur effect on scroll in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,25 @@
 import {navLinks} from "../constants/index.js";
+import gsap from "gsap";
+import {useGSAP} from "@gsap/react";
 
 const Navbar = () => {
+        useGSAP(() => {
+            const navTween = gsap.timeline({
+                scrollTrigger: {
+                    trigger: 'nav',
+                    start: 'bottom top',
+                }
+            });
+
+            navTween.fromTo('nav', {
+                backgroundColor: 'transparent',
+            }, {
+                backgroundColor: 'rgba(0, 0, 0, 0.8)',
+                backdropFilter: 'blur(3px)',
+                ease: 'power1.inOut',
+            });
+        } );
+
     return <nav>
         <div>
             <a href="#home" className='flex items-center gap-2'>
