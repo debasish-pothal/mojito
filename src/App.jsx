7399cfe91d2c4bd2edb91c6fe7500bb81f162c13
Debasish Pothal <debasish_pothal@hotmail.com>
2025-07-07 18:18:00 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import {gsap} from "gsap";
+import {ScrollTrigger, SplitText} from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+function App() {
+  return (
+    <>
+      <h1>Mojito</h1>
+    </>
+  )
+}
+
+export default App
