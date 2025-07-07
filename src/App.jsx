f9839e01aa56e034a1543e707d6389cf18cd19ea
Debasish Pothal <debasish_pothal@hotmail.com>
2025-07-07 19:05:30 +0530

    add required assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import gsap from "gsap";
 import {ScrollTrigger, SplitText} from "gsap/all";
+import Navbar from "./components/Navbar.jsx";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 function App() {
   return (
-    <div className="flex-center h-[100vh]">
-      <h1 className="text-3xl text-indigo-500">Mojito</h1>
-    </div>
+    <main>
+        <Navbar />
+    </main>
   )
 }
 
