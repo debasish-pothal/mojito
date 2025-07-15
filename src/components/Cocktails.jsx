87a1d70b8fbbf489f9fa85027e78a6eae3bff9fd
Debasish Pothal <debasish_pothal@hotmail.com>
2025-07-15 20:58:47 +0530

    added parallax scroll effect on cocktail section

--- src/components/Cocktails.jsx
+++ src/components/Cocktails.jsx
@@ -1,7 +1,38 @@
 import React from "react";
 import { cocktailLists, mockTailLists } from "../constants";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 const Cocktails = () => {
+  useGSAP(() => {
+    const parallaxTimeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#cocktails",
+        start: "top 30%",
+        end: "bottom 80%",
+        scrub: true,
+      },
+    });
+
+    parallaxTimeline
+      .from(
+        "#c-left-leaf",
+        {
+          x: -100,
+          y: 100,
+        },
+        0
+      )
+      .from(
+        "#c-right-leaf",
+        {
+          x: 100,
+          y: 100,
+        },
+        0
+      );
+  }, []);
+
   return (
     <section id="cocktails" className="noisy">
       <img
